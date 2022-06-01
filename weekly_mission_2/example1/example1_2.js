3644da86447f7cb8da2ace77ea9feb67a8f4ecbf
Luis Pasos <kroc545@hotmail.com>
2022-06-01 17:06:18 -0500

    Creación de una funcion y ejecución dentro de un objeto

--- weekly_mission_2/example1/example1_2.js
+++ weekly_mission_2/example1/example1_2.js
@@ -17,3 +17,16 @@ console.log(explorer)
 console.log("Accediendo a las propiedades de un objeto")
 console.log(explorer.name)
 console.log(explorer["age"])
+
+const ajolonauta = {
+    name: "Woopa",
+    mission: "Node JS",
+    sayHelloToExplorers: function(){
+        console.log("Soy el ajolonauta, que onda!")
+    }
+
+}
+
+console.log("Ajolonauta: ")
+console.log(ajolonauta)
+ajolonauta.sayHelloToExplorers()
