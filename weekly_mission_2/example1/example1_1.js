6b93b5922ac1058adecfbde981b59a643112a9f2
Luis Pasos <kroc545@hotmail.com>
2022-06-01 16:56:50 -0500

    Actualizaciones y ejemplos de objetos

---
+++ weekly_mission_2/example1/example1_1.js
@@ -0,0 +1,10 @@
+console.log("Creación de un objeto vacío")
+
+const myFirstObject = {}
+
+console.log("1.- Primera forma de llamada")
+console.log(`Objeto vacio: ${myFirstObject}`) // llamar a un objeto vacio
+
+console.log("2.- Segunda forma de llamada")
+console.log(`Objeto vacio:`) 
+console.log(myFirstObject)
---
+++ weekly_mission_2/example1/example1_2.js
@@ -0,0 +1,18 @@
+console.log("2) Creación de un objeto con propiedades")
+
+const explorer = {
+
+    name: "Luis",
+    age: 31,
+    mission: "NodeJS",
+    stack: ["js", "node", "react", "vue"],
+    blog: {
+        url: "github.com/krocoloco/blog",
+        post: 5
+    } //esto es un objeto
+}
+
+console.log(explorer)
+
+console.log("Accediendo a las propiedades de un objeto")
+console.log(explorer.name)
